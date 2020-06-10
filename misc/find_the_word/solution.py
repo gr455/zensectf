a773bf1c1e0767f1655b53cf0bfc24118d6af127
quantumhacker99 <hemanthx9@gmail.com>
2020-06-10 20:59:56 +0530

    Misc find_word question text updated

--- misc/find_the_word/solution.py
+++ misc/find_the_word/solution.py
@@ -6,7 +6,6 @@ lst = reader.replace("\n", " ").split(" ")
 lst[:] = (value for value in lst if value != '')
 lst[:] = (x.lower() for x in lst)                       #All this is formatting basically, removing punctuations,converting to lowercase, etc. Don't want to explain it all, it is better to understand each                                                         function used for oneself
 s = 'b754015b1f05de447a0915eea5238c1f3310a4826b3190d5b7fe4739bc3bc992'
-print(lst)
 for i in lst:
     word = "zenseCTF{"+i+"}"                             #Wrapping around
     hashval = hashlib.sha256(word.encode()).hexdigest()  
