6b277809d01ede489e08615b9287a8141b389b5c
DaKeiser <rithwiksaim@gmail.com>
2020-06-14 23:22:13 +0530

    Added Crypto stuff

--- crypto/bad_timing/soln.py
+++ crypto/bad_timing/soln.py
@@ -3,11 +3,13 @@ import base64
 import time
 from hashlib import md5
 
-enc = 'FXwrvytE0NLeW9ipw1M4CP2ijMuKv+9/kONY9SsrDhbNtKMa4mRoFGpzwnCUt9EOFNfCXkxFqBHFR8c/535PsxTXwl5MRagRxUfHP+d+T7M='
+enc = '3Y6SUOJw4QCM1fukxt2TWB2eO5giteB2TjzxJudeb2mnQYqLoND2bzU14Fc4hKZGi6Ezmeyh7MeHQT21GwCSDouhM5nsoezHh0E9tRsAkg4='
 
 cipher = base64.b64decode(enc)
 
-for tim in range(int(time.time())-(86400), (int(time.time()))):
+#Change the range max value according to the day of the event you are solving this challenge.
+#I'm assuming that you are doing it on 20th
+for tim in range(int(time.time())-(86400*2), (int(time.time()))):
     key = md5(str(tim)).digest()
     aes = AES.new(key, AES.MODE_ECB)
     plaintext = aes.decrypt(cipher)
