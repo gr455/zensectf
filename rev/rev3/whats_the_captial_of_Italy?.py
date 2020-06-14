2daa66227c8102de99fd11b29655406ab3381713
Kartik-Sama <44060484+Kartik-Sama@users.noreply.github.com>
2020-06-14 16:13:37 +0530

    Reversing 3rd question added!

---
+++ rev/rev3/whats_the_captial_of_Italy?.py
@@ -0,0 +1,54 @@
+def representation(n):
+    l_map = {1000:"M",500:"D",100:"C",50:"L",10:"X",5:"V",1:"I"}
+    if(n >= 4000):
+        return "Bad input"
+    l = []
+    for val in l_map:
+        fr = int(n/val)
+        n = n - (fr*val)
+        while(fr != 0):
+            l.append(l_map[val])
+            fr = fr - 1
+    ind = 0
+    f_l = []
+    for val in l_map:
+        if(l.count(l_map[val]) > 3):
+            if(l.count(l_map[val*5]) == 0):
+                f_l.append(l_map[val])
+                f_l.append(l_map[val*5])
+            else:
+                f_l.pop()
+                f_l.append(l_map[val])
+                f_l.append(l_map[val*10])
+        else:
+            for i in range(l.count(l_map[val])):
+                f_l.append(l_map[val])
+
+    r = ""
+    for i in f_l:
+        r = r + i
+    return r
+
+def container():
+    print("Input key:")
+    ans = input()
+    rstr = ""
+#    print(len(ans))
+    for i in range(0,len(ans),2):
+#       print(i)
+        if(i != len(ans)-1):
+            ad = representation(ord(ans[i])+ord(ans[i+1]))
+            sb = representation(abs(ord(ans[i])-ord(ans[i+1])))
+#            print(ad,sb)
+            if(ord(ans[i]) > ord(ans[i])):
+                rstr = rstr + ad + "s" + sb + "s"
+            else:
+                rstr = rstr + sb + "s" + ad + "s"
+        else:
+            rstr = rstr + representation(ord(ans[i]))
+    if(rstr == "XXIsCCXXIIIsVsCCXXVsXXXIVsCLXVIIIsXIVsCLIVsIXsCCXXXVIIsLXVIIsCLXIXsLXIIIsCLXVsLXIsCLIXsVIIIsCXCVIIIsLIVsCLsXIXsCCIXsLXVIsCLXIIsLVIIsCLXIsXVsCCVsXXXIXsCXCVsLVIIIsCLXsLXIIsCLXVIsLVsCLXIsXCIIsCLVIIIs"):
+        print("Yup that's the key!, Great Job")
+    else:
+        print("Try again!")
+
+container()
