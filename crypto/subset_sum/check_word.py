da50fc70d6402330031ad99a8e4dfc276c52bc8a
quantumhacker99 <hemanthx9@gmail.com>
2020-06-19 12:40:20 +0530

    Subset sum updated

--- crypto/subset_sum/check_word.py
+++ crypto/subset_sum/check_word.py
@@ -15,8 +15,8 @@ def change(l,i,x):
 
 def leet_to_ascii(l):
     for i in l:
-        if i == "1":
-            change(l,i,"l")
+        #if i == "1":
+        #    change(l,i,"l")
         if i == "3":
             change(l,i,"e")
         if i == "4":
@@ -30,8 +30,8 @@ def change_string(s,i,x):
 
 def ascii_to_leet(l):
     for i in range(len(l)):
-        if l[i] == "l":
-            l = change_string(l,i,"1")
+        #if l[i] == "l":
+        #    l = change_string(l,i,"1")
             #print(f'This is {l}')
         if l[i] == "e":
             l = change_string(l,i,"3")
@@ -40,15 +40,21 @@ def ascii_to_leet(l):
         if l[i] == "s":
             l = change_string(l,i,"5")
     return l
+s_list = []
 #print(ascii_to_leet("1b3"))
-while t != "-1":
+while True:
     t = input()
-    if t == -1:
+    if t == "-1":
+        print(s_list)
         break
     l = mapp(int,t.split())
     l.reverse()
     l = mapp(chr,l)
     l = leet_to_ascii(l)
+    l4 =[53,109,52,108,108,95,107,51,121]
+    #l4.reverse()
+    if l == l4:
+        print(l)
     if "_" in l:
         s1 = ""
         s2 = ""
@@ -61,9 +67,9 @@ while t != "-1":
         if len(s1) == 0 or len(s2) == 0:
             continue
         if d.check(s1) and d.check(s2):
-            s3 = ascii_to_leet(s1+s2)
-            print(s3) #ascii_to_leet(s1+s2))
+            s3 = ascii_to_leet(s1+"_"+s2)
+            s_list.append("zenseCTF{"+s3+"}") #ascii_to_leet(s1+s2))
             #print(s2)
-
-
-
+        #else:
+            #print(s1)
+            #print(s2)
